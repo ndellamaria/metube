901a4fb529f6cb28ab1ee25da608e27b5e5b4211
ndellamaria <nrdell3@gmail.com>
2019-02-22 14:08:57 -0500

    added buffet database user info

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$database = "metube";
+$dbhost = "mysql1.cs.clemson.edu";
+$dbuser = "metube_1rim";
+$dbpass = "cpsc4620";
+$database = "metube_nky9";
 ?>
