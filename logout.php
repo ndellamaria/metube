d4aeb500ac333981f018f7c4200310025790086b
Clay Dombrowski <cldombr@g.clemson.edu>
2019-04-18 10:16:16 -0400

    Added top nav bar to logout page

--- logout.php
+++ logout.php
@@ -1,5 +1,28 @@
+<head>
+<title>Profile</title>
+<link rel="stylesheet" type="text/css" href="default.css" />
+</head>
+
+<body>
+<div class="topnav">
+  <a class="active" href="browse.php">MeTube</a>
+  <?php
+	if (! empty($_SESSION['logged_in']))
+	{
+  		echo "<a href='logout.php'>Logout</a>
+  		<a href='update.php'>Profile</a>";
+	}
+	else {
+		echo"<a href='index.php'>Login</a>";
+		echo"<a href='registration.php'>Register</a>";
+	}
+  ?>
+</div>
+</body>
+
+
 <?php
 session_start();
 session_destroy();
-echo 'You have been logged out. <a href="index.php">Login</a>';
+echo '<div>You have been logged out. <a href="index.php">Login</a> </div>';
 ?>
